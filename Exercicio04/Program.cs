4c20425fed5ac378b48b60d5f5be57c70efceb28
Iagodizlla <iagoschlemper@gmail.com>
2025-03-20 14:52:43 -0300

    Implementado exercio 04

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio04
+{
+    internal class Program
+    {
+        //Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos Gruas Celsius: ");
+            double GC = double.Parse(Console.ReadLine()!);
+
+            double GF = GC * 1.8 + 32;
+            Console.WriteLine($"Isso equivale a {GF} Graus Fahrenheit");
+            Console.ReadLine();
+        }
+    }
+}
