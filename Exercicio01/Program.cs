843aef2438f0cca9c7ac4de7bba285f18cd3234c
Iagodizlla <iagoschlemper@gmail.com>
2025-03-20 14:35:20 -0300

    Implementado exercicio 01

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio01
+{
+    internal class Program
+    {
+        //Crie um programa para calcular o volume de uma caixa retangular
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a altura da caixa: ");
+            double altura = double.Parse(Console.ReadLine()!);
+            Console.Write("Qual a largura da caixa: ");
+            double largura = double.Parse(Console.ReadLine()!);
+            Console.Write("Qual o comprimento da caixa: ");
+            double comprimento = double.Parse(Console.ReadLine()!);
+
+            double volume = altura * largura * comprimento;
+            Console.WriteLine($"Volume: {volume}(cm cubinos)");
+            Console.ReadLine();
+        }
+    }
+}
