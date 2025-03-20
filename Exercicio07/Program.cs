9fc76c37bbd5a98704c10f2787c93825d86c5761
Iagodizlla <iagoschlemper@gmail.com>
2025-03-20 15:23:15 -0300

    implementado exercicio 07

---
+++ Exercicio07/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio07
+{
+    internal class Program
+    {
+        //Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a nota do aluno e seu peso?");
+            double n1 = double.Parse(Console.ReadLine()!);
+            int p1 = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Qual a segunda nota do aluno e seu peso?");
+            double n2 = double.Parse(Console.ReadLine()!);
+            int p2 = int.Parse(Console.ReadLine()!);
+
+            double MP = (p1 * n1 + p2 * n2) / p1 + p2;
+            Console.WriteLine($"Media Ponderada: {MP}");
+            Console.ReadLine();
+        }
+    }
+}
