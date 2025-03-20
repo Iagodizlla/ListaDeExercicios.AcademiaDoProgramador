043f33de01e98b38a241a97416e46c1e9be738a0
Iagodizlla <iagoschlemper@gmail.com>
2025-03-20 14:48:10 -0300

    Implementado exercicio 03

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio03
+{
+    internal class Program
+    {
+        /*Crie um programa que calcule o consumo de combustível por quilômetro percorrido em uma viagem. O
+        programa deve solicitar ao usuário:
+        ● A quilometragem inicial do veículo no início da viagem.
+        ● A quilometragem final ao término da viagem.
+        ● A quantidade de combustível consumida durante a viagem (em litros).*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o km inicial: ");
+            double kmI = double.Parse(Console.ReadLine()!);
+            Console.WriteLine("Qual o km final: ");
+            double kmF = double.Parse(Console.ReadLine()!);
+            Console.WriteLine("Quanto de conbustivel foi gasto(litros): ");
+            double ComG = double.Parse(Console.ReadLine()!);
+
+            double ComKm = (kmF - kmI) / ComG;
+            Console.WriteLine($"O carro gasta {ComKm} de combustivel por km");
+            Console.ReadLine();
+        }
+    }
+}
