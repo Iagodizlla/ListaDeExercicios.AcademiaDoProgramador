7348c12e27a0e6af16e32ee1d6d79a427becfdf0
Iagodizlla <iagoschlemper@gmail.com>
2025-03-20 15:38:07 -0300

    implementado exercicio 09

---
+++ Exercicio09/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio09
+{
+    internal class Program
+    {
+        /*A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um algoritmo para ler as dimensões de um
+        terreno e depois exibir a área do terreno.*/
+        static void Main(string[] args)
+        {
+            Console.Write("Qual o comprimento do terreno(metros): ");
+            double comprimento = double.Parse(Console.ReadLine()!);
+            Console.Write("Qual a largura do terreno(metros): ");
+            double largura = double.Parse(Console.ReadLine()!);
+
+            double area = largura * comprimento;
+            Console.WriteLine($"Area do terreno: {area} metros quadrados");
+        }
+    }
+}
