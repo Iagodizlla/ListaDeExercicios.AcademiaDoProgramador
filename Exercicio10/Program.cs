fa08489cbae5d8f04f558bd45444e105bbc83738
Iagodizlla <iagoschlemper@gmail.com>
2025-03-20 15:47:16 -0300

    Implementado exercicio 10

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicio10
+{
+    internal class Program
+    {
+        /*A padaria Hotpão vende uma certa quantidade de pães franceses e uma quantidade de broas a cada dia. Cada
+        pãozinho custa R$ 0,12 e a broa custa R$ 1,50. Ao final do dia, o dono quer saber quanto arrecadou com a
+        venda dos pães e broas (juntos), e quanto deve guardar numa conta de poupança (10% do total arrecadado).
+        Você foi contratado para fazer os cálculos para o dono. Com base nestes fatos, faça um algoritmo para ler as
+        quantidades de pães e de broas, e depois calcular os dados solicitados.*/
+        static void Main(string[] args)
+        {
+            Console.Write("Qunatos pães foram vendidos: ");
+            double qp = double.Parse(Console.ReadLine()!);
+            Console.Write("Qunatas broas foram vendidas: ");
+            double qb = double.Parse(Console.ReadLine()!);
+
+            qp *= 0.12;
+            qb *= 1.5;
+            double gp = (double)(qp + qb) * 0.1;
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Arrecadacao");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine($"Pães: R${qp}");
+            Console.WriteLine($"Broas: R${qb}");
+            Console.WriteLine($"Guardar na poupanca: R${gp}");
+            Console.ReadLine();
+        }
+    }
+}
