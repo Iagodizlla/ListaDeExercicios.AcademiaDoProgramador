d66e57f09e2bc13aac3a500aba3a074ee372314e
Iagodizlla <iagoschlemper@gmail.com>
2025-03-20 15:33:45 -0300

    Implementado exercicio 08

---
+++ Exercicio08/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercicio08
+{
+    internal class Program
+    {
+        //Crie um programa para verificar se um número é primo.
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            int n = int.Parse(Console.ReadLine()!);
+            int cont = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    cont++;
+
+                }
+            }
+
+            if(cont == 1 || n == 1)
+            {
+                Console.WriteLine("E um numero primo!");
+            }
+            else
+            {
+                Console.WriteLine("Nao e um numero primo!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
