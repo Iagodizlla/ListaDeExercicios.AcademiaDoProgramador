e8b45c7f9f34665fae802739a4fa02e886bae28a
Iagodizlla <iagoschlemper@gmail.com>
2025-03-20 16:45:02 -0300

    Implementado exercicio 16

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio16
+{
+    internal class Program
+    {
+        //Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            double n = int.Parse(Console.ReadLine()!);
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
