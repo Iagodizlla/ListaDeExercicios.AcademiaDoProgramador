3c68c46b565a7ac18b0187441d4cd15ea0cf971e
Iagodizlla <iagoschlemper@gmail.com>
2025-03-20 16:55:59 -0300

    implementado exercicio 19

---
+++ Exercicio19/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio19
+{
+    internal class Program
+    {
+        /*Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de três e que se
+        encontram no conjunto dos números de 1 até 500.*/
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 500; i++)
+            {
+                if (i % 2 == 1 && i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
