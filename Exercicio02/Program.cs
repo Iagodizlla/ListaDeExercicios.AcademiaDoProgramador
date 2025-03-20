8b74c912da89a1ed5a54c6d079e7d1d99d76bddb
Iagodizlla <iagoschlemper@gmail.com>
2025-03-20 14:42:13 -0300

    implementado exercicio 02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        //Crie um programa para calcular o volume de um Cilindro
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o raio da base do cilindro: ");
+            double raio = double.Parse(Console.ReadLine()!);
+            Console.WriteLine("Qual a altura do cilindro:");
+            double altura = double.Parse(Console.ReadLine()!);
+
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+            Console.WriteLine($"Volume do colindro: {volume}");
+            Console.ReadLine();
+        }
+    }
+}
